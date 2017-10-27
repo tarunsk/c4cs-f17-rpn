9b8694882094e7750442d0915c68efe8ca285032
Tarun Khubchandani <tarun@kfit.com>
2017-10-27 12:37:09 -0400

    RPN Calculator from lecture

--- tdd/rpn.py
+++ tdd/rpn.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
+import operator
 
+ops = {
+    '+': operator.add,
+    '-': operator.sub
+}
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-        if token == '+':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 + arg2
-            stack.append(result)
-        elif token == '-':
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = arg1 - arg2
-            stack.append(result)
-        else:
+        try:
             stack.append(int(token))
-    # print(stack)
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            result = function(arg1, arg2)
+            stack.append(result)
     return stack.pop()
 
 def main():
